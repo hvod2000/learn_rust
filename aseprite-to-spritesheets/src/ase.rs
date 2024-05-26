c79f7a7392ba0eada22d5514be1203434683305c
Uladzislau Khamkou <kham@tuta.io>
2024-05-26 23:41:33 +0300

    Add my own abstraction layer above aseprite reader

---
+++ aseprite-to-spritesheets/src/ase.rs
@@ -0,0 +1,51 @@
+use std::io::{Error, Result};
+
+pub struct Aseprite {
+	pub width: usize,
+	pub height: usize,
+	pub tags: Vec<Tag>,
+	pub frames: Vec<Vec<Vec<[u8; 4]>>>,
+
+	file: aseprite_reader2::Aseprite,
+}
+
+#[derive(Debug, Clone)]
+pub struct Tag {
+	pub name: String,
+	pub start: usize,
+	pub end: usize,
+}
+
+impl Aseprite {
+	pub fn load<P: AsRef<std::path::Path>>(path: P) -> Result<Self> {
+		let Ok(file) = aseprite_reader2::Aseprite::from_path(path) else {
+			return Err(Error::other("Failed to read aseprite file"));
+		};
+		let tags = file
+			.tags()
+			.all()
+			.map(|tag| Tag {
+				name: tag.name.clone(),
+				start: tag.frames.start as usize,
+				end: tag.frames.end as usize,
+			})
+			.collect();
+		let frames_count = file.frames().count();
+		let frames = file.frames().get_for(&(0..(frames_count as u16))).get_images().unwrap();
+		let width = frames[0].width() as usize;
+		let height = frames[0].height() as usize;
+		let frames =
+			Vec::from_iter(frames.into_iter().map(|image| {
+				Vec::from_iter((0..height).map(|y| {
+					Vec::from_iter((0..width).map(|x| image.get_pixel(x as u32, y as u32).0))
+				}))
+			}));
+		Ok(Aseprite { width, height, tags, frames, file })
+	}
+}
+
+impl Tag {
+	pub fn idle(frames: usize) -> Self {
+		Tag { name: "Idle".to_string(), start: 0, end: frames }
+	}
+}
--- aseprite-to-spritesheets/src/main.rs
+++ aseprite-to-spritesheets/src/main.rs
@@ -1,11 +1,15 @@
 #![allow(dead_code, unused_mut, unused_variables, unused_assignments)]
+use std::io::Result;
 use std::path::{Path, PathBuf};
 use std::process::exit;
+mod ase;
 
-use aseprite_reader2::Aseprite;
+use ase::Aseprite;
 use image::{self, Rgba};
 
-fn main() {
+use crate::ase::Tag;
+
+fn main() -> Result<()> {
 	let args: Vec<String> = std::env::args().collect();
 	if args.len() < 2 || args.len() > 3 {
 		eprintln!("Usage: {} SOURCE_DIR [TARGET_DIR]", args[0]);
@@ -28,37 +32,38 @@ fn main() {
 		let path = source.strip_prefix(&source_dir).unwrap();
 		let target = target_dir.join(&path);
 		// println!("{} -> {}", source.to_string_lossy(), target.to_string_lossy());
-		export(&source_dir, &source);
+		export(&source_dir, &source)?;
 	}
+	Ok(())
 	// save_thumbnail(&args[1], &args[2]);
 }
 
-fn export(source_dir: &Path, ase_path: &Path) {
+fn export(source_dir: &Path, ase_path: &Path) -> Result<()> {
 	println!("{}", ase_path.to_string_lossy());
-	let ase = Aseprite::from_path(ase_path).expect("Failed to read aseprite file");
-	let tags = &ase.tags();
-	let tags: Vec<_> = tags.all().collect();
-    let frames = &ase.frames();
-	if tags.len() > 0 || frames.count() > 0 {
-        println!("animation");
-        // TODO: I actually need better aseprite reader
+	let ase = Aseprite::load(ase_path)?;
+	let tags = ase.tags.to_vec();
+	let frames = ase.frames.to_vec();
+	if tags.len() > 0 || frames.len() > 0 {
+		let tags = if tags.len() > 0 { tags } else { vec![Tag::idle(frames.len())] };
+		println!("\tAnimation with {} tags and {} frames", tags.len(), frames.len());
 	} else {
-        println!("static sprite");
+		println!("static sprite");
 	}
+	Ok(())
 }
 
-fn save_thumbnail(input: &str, output: &str) {
-	let aseprite = Aseprite::from_path(input).expect("Could not read aseprite file.");
-	let frames = aseprite.frames();
-	let image = &frames.get_for(&(0..1)).get_images().unwrap()[0];
-	let w = image.width();
-	let h = image.height();
-	let s = 128;
-	let mut thumbnail = image::ImageBuffer::new(s, s);
-
-	for (x, y, pixel) in thumbnail.enumerate_pixels_mut() {
-		let color = image.get_pixel(x * w / s, y * h / s).0;
-		*pixel = Rgba(color);
-	}
-	thumbnail.save(output).expect("Failed to save thumbnail file");
-}
+// fn save_thumbnail(input: &str, output: &str) {
+// 	let aseprite = Aseprite::from_path(input).expect("Could not read aseprite file.");
+// 	let frames = aseprite.frames();
+// 	let image = &frames.get_for(&(0..1)).get_images().unwrap()[0];
+// 	let w = image.width();
+// 	let h = image.height();
+// 	let s = 128;
+// 	let mut thumbnail = image::ImageBuffer::new(s, s);
+//
+// 	for (x, y, pixel) in thumbnail.enumerate_pixels_mut() {
+// 		let color = image.get_pixel(x * w / s, y * h / s).0;
+// 		*pixel = Rgba(color);
+// 	}
+// 	thumbnail.save(output).expect("Failed to save thumbnail file");
+// }
